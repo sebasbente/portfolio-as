0be336d2b1af01e27a36a0bd5fb226453f71a1bf
sebasbente <sebasbentedg@gmail.com>
2022-11-28 10:46:52 -0300

    navbar terminado

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,40 +1,48 @@
-import { AppBar, Drawer, IconButton, List, makeStyles, Toolbar } from "@material-ui/core";
+import { AppBar, Divider, Drawer, IconButton, List, ListItem, ListItemIcon, makeStyles, Toolbar } from "@material-ui/core";
 import React, { useState } from "react";
 import logo from "./images/full-stack.png"
 import { Link, animateScroll as scroll } from "react-scroll";
-import InfoTwoToneIcon from "@material-ui/icons/InfoTwoTone";
-import EmojiObjectsTwoToneIcon from "@material-ui/icons/InfoTwoTone";
-import BuildTwoToneIcon from "@material-ui/icons/InfoTwoTone";
-import ContactMailTwoToneIcon from "@material-ui/icons/InfoTwoTone";
+
+//import InfoTwoToneIcon from "@material-ui/icons/InfoTwoTone";
+//import EmojiObjectsTwoToneIcon from "@material-ui/icons/InfoTwoTone";
+//import BuildTwoToneIcon from "@material-ui/icons/InfoTwoTone";
+//import ContactMailTwoToneIcon from "@material-ui/icons/InfoTwoTone";
+
+import InfoTwoToneIcon from '@mui/icons-material/InfoTwoTone';
+import EmojiObjectsTwoToneIcon from '@mui/icons-material/EmojiObjectsTwoTone';
+import BuildTwoToneIcon from '@mui/icons-material/BuildTwoTone';
+import ContactMailTwoToneIcon from '@mui/icons-material/ContactMailTwoTone';
+
+
 import MenuIcon from "@material-ui/icons/Menu";
 import CancelIcon from "@material-ui/icons/Cancel";
 
-const links = [
-    {
-        id: "about",
-        text: "About Me",
-        icon: <InfoTwoToneIcon/>
-    },
-    {
-        id: "skills",
-        text: "Skills",
-        icon: <EmojiObjectsTwoToneIcon/>
-    },
-    {
-        id: "work",
-        text: "My Work",
-        icon: <BuildTwoToneIcon/>
-    },
-    {
-        id: "contact",
-        text: "Get in touch",
-        icon: <ContactMailTwoToneIcon/>
-    },
-]
 
 const NavBar = () => {
     const classes = useStyles();
-    const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(true);
+    const links = [
+        {
+            id: "about",
+            text: "About Me",
+            icon: <InfoTwoToneIcon fontSize="large"/>
+        },
+        {
+            id: "skills",
+            text: "Skills",
+            icon: <EmojiObjectsTwoToneIcon fontSize="large" className={classes.light}/>
+        },
+        {
+            id: "work",
+            text: "My Work",
+            icon: <BuildTwoToneIcon fontSize="large"/>
+        },
+        {
+            id: "contact",
+            text: "Get in touch",
+            icon: <ContactMailTwoToneIcon fontSize="large"/>
+        },
+    ]
     return (
     <>
         <AppBar position="sticky" className={classes.root}>
@@ -55,16 +63,41 @@ const NavBar = () => {
                         ))
                     }
                 </List>
-                <IconButton edge="end" className={classes.menubutton }>
+                <IconButton edge="end" 
+                    className={classes.menubutton }
+                    onClick={()=>setOpen(!open)}
+                >
                     <MenuIcon fontSize="large"/>
                 </IconButton>
                 
             </Toolbar>
         </AppBar>
         <Drawer anchor="right" open={open} onClose={()=>setOpen(false)}>
-            <IconButton>
-                <CancelIcon/>
+            <IconButton onClick={()=>setOpen(false)} className={classes.cancelicon}>
+                <CancelIcon fontSize="large"/>
             </IconButton>
+            <Divider/>
+                {
+                    links.map(({ id, text, icon }, index)=>(
+                        <Link key={index} 
+                            className={classes.sidebar}
+                            to={id} 
+                            spy={true} 
+                            activeClass="active" 
+                            smooth={true} 
+                            duration={500} 
+                            offset={-70}>
+                            <ListItem component="h5">
+                                <span>
+                                    <ListItemIcon>
+                                        {icon}
+                                    </ListItemIcon>
+                                </span>{text}
+                            </ListItem>
+                            </Link>
+                        ))
+                    }
+            
         </Drawer>
     </>
 
@@ -91,6 +124,9 @@ const useStyles = makeStyles((theme)=> ({
             cursor: "pointer"
         }  
     },
+    light: {
+        color: "#ffcc00",
+    },
     menu: {
         [theme.breakpoints.down("sm")]: {
             display: "none"
@@ -115,6 +151,28 @@ const useStyles = makeStyles((theme)=> ({
             position: "absolute",
             top: 0,
             right: 10,
+        }
+    },
+    cancelicon: {
+        color: "tomato",
+        position: "absolute",
+        top: 0,
+        right: 10,
+    },
+    sidebar: {
+        width: "40vw",
+        [theme.breakpoints.down("sm")]: {
+            width: "60vw",
+        },
+        "& h5": {
+            margin: theme.spacing(6,0,0,4),
+            fontSize: "1.4rem",
+            color: "#333",
+            fontWeight: "bold",
+        },
+        "& h5:hover": {
+            color: "tomato",
+            cursor: "pointer",
         }
     }
   }))
